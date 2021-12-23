898f321df19f10b6591d89164eaef5edd4917c62
xxx <sfonline456@gmail.com>
2021-12-23 22:50:23 +0800

    hw5

---
+++ HW5JAVA/test_p14/src/pb/Sample.java
@@ -0,0 +1,11 @@
+package pb;
+import pc.Car;
+
+class Sample 
+{
+	public static void main(String[] args)
+	{
+		Car car1 = new Car();
+		car1.show();
+	}
+}
---
+++ HW5JAVA/test_p6/src/Sample.java
@@ -0,0 +1,8 @@
+class Sample 
+{
+	public static void main(String[] args) {
+		Car car1;
+		car1 = new Car();
+		car1.show();
+	}
+}
